469865d51bc9323f328e28e88da0efdee019bf83
farizalmsyh <farizalamsyah22@gmail.com>
2022-01-28 13:53:36 +0700

    fix routing

--- resources/views/layouts/masternew.blade.php
+++ resources/views/layouts/masternew.blade.php
@@ -12,24 +12,24 @@
         <meta name="author" content="keenthemes.com">
 
         <!-- Web Fonts -->
-        <link href={{asset("https://fonts.googleapis.com/css?family=Lato:300,400,400i|Montserrat:400,700")}} rel="stylesheet")>
+        <link href="{{asset('https://fonts.googleapis.com/css?family=Lato:300,400,400i|Montserrat:400,700')}}" rel="stylesheet")>
 
         <!-- Vendor Styles -->
-        <link href={{asset("compro/vendor/bootstrap/css/bootstrap.min.css")}} rel="stylesheet" type="text/css"/>
-        <link href={{asset("compro/css/animate.css")}} rel="stylesheet") type="text/css"/>
-        <link href={{asset("compro/vendor/themify/themify.css")}} rel="stylesheet" type="text/css"/>
-        <link href={{asset("compro/vendor/scrollbar/scrollbar.min.css")}} rel="stylesheet" type="text/css"/>
-        <link href={{asset("compro/vendor/magnific-popup/magnific-popup.css")}} rel="stylesheet" type="text/css"/>
-        <link href={{asset("compro/vendor/swiper/swiper.min.css")}} rel="stylesheet" type="text/css"/>
-        <link href={{asset("compro/vendor/cubeportfolio/css/cubeportfolio.min.css")}} rel="stylesheet" type="text/css"/>
+        <link href="{{asset('compro/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css"/>
+        <link href="{{asset('compro/css/animate.css')}}" rel="stylesheet") type="text/css"/>
+        <link href="{{asset('compro/vendor/themify/themify.css')}}" rel="stylesheet" type="text/css"/>
+        <link href="{{asset('compro/vendor/scrollbar/scrollbar.min.css')}}" rel="stylesheet" type="text/css"/>
+        <link href="{{asset('compro/vendor/magnific-popup/magnific-popup.css')}}" rel="stylesheet" type="text/css"/>
+        <link href="{{asset('compro/vendor/swiper/swiper.min.css')}}" rel="stylesheet" type="text/css"/>
+        <link href="{{asset('compro/vendor/cubeportfolio/css/cubeportfolio.min.css')}}" rel="stylesheet" type="text/css"/>
 
         <!-- Theme Styles -->
-        <link href={{asset("compro/css/style.css")}} rel="stylesheet" type="text/css"/>
-        <link href={{asset("compro/css/global/global.css")}} rel="stylesheet" type="text/css"/>
+        <link href="{{asset('compro/css/style.css')}}" rel="stylesheet" type="text/css"/>
+        <link href="{{asset('compro/css/global/global.css')}}" rel="stylesheet" type="text/css"/>
 
         <!-- Favicon -->
-        <link rel="shortcut icon" href={{asset("compro/img/favicon.ico")}} type="image/x-icon">
-        <link rel="apple-touch-icon" href={{asset("compro/img/apple-touch-icon.png")}}>
+        <link rel="shortcut icon" href="{{asset('compro/img/favicon.ico')}}" type="image/x-icon">
+        <link rel="apple-touch-icon" href="{{asset('compro/img/apple-touch-icon.png')}}">
     </head>
     <!-- End Head -->
 
@@ -45,9 +45,9 @@
                         <div class="s-header__navbar-row-col">
                             <!-- Logo -->
                             <div class="s-header__logo">
-                                <a href={{url("indexnew")}} class="s-header__logo-link">
-                                    <img class="s-header__logo-img s-header__logo-img-default" src={{asset("compro/img/logo.png")}} alt="Megakit Logo">
-                                    <img class="s-header__logo-img s-header__logo-img-shrink" src={{("compro/img/logo-dark.png")}} alt="Megakit Logo">
+                                <a href="{{url('/')}}" class="s-header__logo-link">
+                                    <img class="s-header__logo-img s-header__logo-img-default" src="{{asset('compro/img/logo.png')}}" alt="Megakit Logo">
+                                    <img class="s-header__logo-img s-header__logo-img-shrink" src="{{('compro/img/logo-dark.png')}}" alt="Megakit Logo">
                                 </a>
                             </div>
                             <!-- End Logo -->
@@ -75,8 +75,8 @@
 
                         <!-- Menu List -->                                
                         <ul class="list-unstyled s-header__nav-menu">
-                            <li class="s-header__nav-menu-item"><a class="s-header__nav-menu-link s-header__nav-menu-link-divider" href={{url("contactnew")}}>Contacts</a></li>
-                            <li class="s-header__nav-menu-item"><a class="s-header__nav-menu-link s-header__nav-menu-link-divider" href={{url("aboutnew")}}>About</a></li>
+                            <li class="s-header__nav-menu-item"><a class="s-header__nav-menu-link s-header__nav-menu-link-divider" href="{{url('contact')}}">Contacts</a></li>
+                            <li class="s-header__nav-menu-item"><a class="s-header__nav-menu-link s-header__nav-menu-link-divider" href="{{url('about')}}">About</a></li>
                         </ul>
                         <!-- End Menu List -->
                     </div>
@@ -104,9 +104,9 @@
                     <div class="row">
                         <div class="col-sm-2 g-margin-b-20--xs g-margin-b-0--md">
                             <ul class="list-unstyled g-ul-li-tb-5--xs g-margin-b-0--xs">
-                                <li><a class="g-font-size-15--xs g-color--white-opacity" href={{url("indexnew")}}>Home</a></li>
-                                <li><a class="g-font-size-15--xs g-color--white-opacity" href={{url("contactnew")}}>About</a></li>
-                                <li><a class="g-font-size-15--xs g-color--white-opacity" href={{url("contactnew")}}>Contact</a></li>
+                                <li><a class="g-font-size-15--xs g-color--white-opacity" href="{{url('/')}}">Home</a></li>
+                                <li><a class="g-font-size-15--xs g-color--white-opacity" href="{{url('/about')}}">About</a></li>
+                                <li><a class="g-font-size-15--xs g-color--white-opacity" href="{{url('/contact')}}">Contact</a></li>
                             </ul>
                         </div>
                         <div class="col-sm-2 g-margin-b-20--xs g-margin-b-0--md">
@@ -129,12 +129,12 @@
                 <div class="row">
                     <div class="col-lg-4 col-md-4 col-xs-12">
                         <a href="index.html">
-                            <img class="g-width-100--xs g-height-auto--xs" src={{asset("compro/img/logo.png")}} alt="Megakit Logo">
+                            <img class="g-width-100--xs g-height-auto--xs" src="{{asset('compro/img/logo.png')}}" alt="Megakit Logo">
                         </a>
                     </div>
                     
                     <div class="col-lg-4 col-md-4 col-xs-12 g-text-left--xs">
-                        <p class="g-font-size-14--xs g-margin-b-0--xs g-color--white-opacity-light"><a href={{("index")}}>Megakit</a> Theme Powered by: <a href="http://www.keenthemes.com/">KeenThemes.com</a></p>
+                        <p class="g-font-size-14--xs g-margin-b-0--xs g-color--white-opacity-light"><a href="{{('/')}}">Megakit</a> Theme Powered by: <a href="http://www.keenthemes.com/">KeenThemes.com</a></p>
                     </div>
                 </div>
             </div>
@@ -147,32 +147,32 @@
 
         <!--========== JAVASCRIPTS (Load javascripts at bottom, this will reduce page load time) ==========-->
         <!-- Vendor -->
-        <script type="text/javascript" src={{asset("compro/vendor/jquery.min.js")}}></script>
-        <script type="text/javascript" src={{asset("compro/vendor/jquery.migrate.min.js")}}></script>
-        <script type="text/javascript" src={{asset("compro/vendor/bootstrap/js/bootstrap.min.js")}}></script>
-        <script type="text/javascript" src={{asset("compro/vendor/jquery.smooth-scroll.min.js")}}></script>
-        <script type="text/javascript" src={{asset("compro/vendor/jquery.back-to-top.min.js")}}></script>
-        <script type="text/javascript" src={{asset("compro/vendor/scrollbar/jquery.scrollbar.min.js")}}></script>
-        <script type="text/javascript" src={{asset("compro/vendor/magnific-popup/jquery.magnific-popup.min.js")}}></script>
-        <script type="text/javascript" src={{asset("compro/vendor/swiper/swiper.jquery.min.js")}}></script>
-        <script type="text/javascript" src={{asset("compro/vendor/waypoint.min.js")}}></script>
-        <script type="text/javascript" src={{asset("compro/vendor/counterup.min.js")}}></script>
-        <script type="text/javascript" src={{asset("compro/vendor/cubeportfolio/js/jquery.cubeportfolio.min.js")}}></script>
-        <script type="text/javascript" src={{asset("compro/vendor/jquery.parallax.min.js")}}></script>
-        <script src={{asset("https://maps.googleapis.com/maps/api/js?key=AIzaSyBsXUGTFS09pLVdsYEE9YrO2y4IAncAO2U")}}></script>
-        <script type="text/javascript" src="vendor/jquery.wow.min.js"></script>
+        <script type="text/javascript" src="{{asset('compro/vendor/jquery.min.js')}}"></script>
+        <script type="text/javascript" src="{{asset('compro/vendor/jquery.migrate.min.js')}}"></script>
+        <script type="text/javascript" src="{{asset('compro/vendor/bootstrap/js/bootstrap.min.js')}}"></script>
+        <script type="text/javascript" src="{{asset('compro/vendor/jquery.smooth-scroll.min.js')}}"></script>
+        <script type="text/javascript" src="{{asset('compro/vendor/jquery.back-to-top.min.js')}}"></script>
+        <script type="text/javascript" src="{{asset('compro/vendor/scrollbar/jquery.scrollbar.min.js')}}"></script>
+        <script type="text/javascript" src="{{asset('compro/vendor/magnific-popup/jquery.magnific-popup.min.js')}}"></script>
+        <script type="text/javascript" src="{{asset('compro/vendor/swiper/swiper.jquery.min.js')}}"></script>
+        <script type="text/javascript" src="{{asset('compro/vendor/waypoint.min.js')}}"></script>
+        <script type="text/javascript" src="{{asset('compro/vendor/counterup.min.js')}}"></script>
+        <script type="text/javascript" src="{{asset('compro/vendor/cubeportfolio/js/jquery.cubeportfolio.min.js')}}"></script>
+        <script type="text/javascript" src="{{asset('compro/vendor/jquery.parallax.min.js')}}"></script>
+        <script src="{{asset('https://maps.googleapis.com/maps/api/js?key=AIzaSyBsXUGTFS09pLVdsYEE9YrO2y4IAncAO2U')}}"></script>
+        <script type="text/javascript" src="{{asset('compro/vendor/jquery.wow.min.js')}}"></script>
 
         <!-- General Components and Settings -->
-        <script type="text/javascript" src={{asset("compro/js/global.min.js")}}></script>
-        <script type="text/javascript" src={{asset("compro/js/components/header-sticky.min.js")}}></script>
-        <script type="text/javascript" src={{asset("compro/js/components/scrollbar.min.js")}}></script>
-        <script type="text/javascript" src={{asset("compro/js/components/magnific-popup.min.js")}}></script>
-        <script type="text/javascript" src={{asset("compro/js/components/swiper.min.js")}}></script>
-        <script type="text/javascript" src={{asset("compro/js/components/counter.min.js")}}></script>
-        <script type="text/javascript" src={{asset("compro/js/components/portfolio-3-col.min.js")}}></script>
-        <script type="text/javascript" src={{asset("compro/js/components/parallax.min.js")}}></script>
-        <script type="text/javascript" src={{asset("compro/js/components/google-map.min.js")}}></script>
-        <script type="text/javascript" src={{asset("compro/js/components/wow.min.js")}}></script>
+        <script type="text/javascript" src="{{asset('compro/js/global.min.js')}}"></script>
+        <script type="text/javascript" src="{{asset('compro/js/components/header-sticky.min.js')}}"></script>
+        <script type="text/javascript" src="{{asset('compro/js/components/scrollbar.min.js')}}"></script>
+        <script type="text/javascript" src="{{asset('compro/js/components/magnific-popup.min.js')}}"></script>
+        <script type="text/javascript" src="{{asset('compro/js/components/swiper.min.js')}}"></script>
+        <script type="text/javascript" src="{{asset('compro/js/components/counter.min.js')}}"></script>
+        <script type="text/javascript" src="{{asset('compro/js/components/portfolio-3-col.min.js')}}"></script>
+        <script type="text/javascript" src="{{asset('compro/js/components/parallax.min.js')}}"></script>
+        <script type="text/javascript" src="{{asset('compro/js/components/google-map.min.js')}}"></script>
+        <script type="text/javascript" src="{{asset('compro/js/components/wow.min.js')}}"></script>
         <!--========== END JAVASCRIPTS ==========-->
 
     </body>
--- routes/web.php
+++ routes/web.php
@@ -13,37 +13,35 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [App\Http\Controllers\HomeController::class, 'welcome'])->name('welcome');
 
-Route::get('/indexnew',[App\Http\Controllers\HomeController::class, 'indexnew'])->name('homenew');
+Route::get('/',[App\Http\Controllers\HomeController::class, 'indexnew'])->name('homenew');
 
-Route::get('/contactnew',[App\Http\Controllers\HomeController::class, 'contactnew'])->name('contactnew');
+Route::get('/contact',[App\Http\Controllers\HomeController::class, 'contactnew'])->name('contactnew');
 
-Route::get('aboutnew',[App\Http\Controllers\HomeController::class, 'aboutnew'])->name('aboutnew');
+Route::get('/about',[App\Http\Controllers\HomeController::class, 'aboutnew'])->name('aboutnew');
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::prefix('admin')->group(function(){
+	Route::get('/', [App\Http\Controllers\HomeController::class, 'welcome'])->name('home');
+	Route::prefix('karyawan')->group(function(){
+		Route::get('/', [App\Http\Controllers\KaryawanController::class, 'index'])->name('karyawan');
+		Route::get('create', [App\Http\Controllers\KaryawanController::class, 'create'])->name('karyawan.create');
+		Route::post('save', [App\Http\Controllers\KaryawanController::class, 'save'])->name('karyawan.save');
+		Route::get('edit/{id}', [App\Http\Controllers\KaryawanController::class, 'edit'])->name('karyawan.edit');
+		Route::post('update/{id}', [App\Http\Controllers\KaryawanController::class, 'update'])->name('karyawan.update');
+		Route::get('delete/{id}', [App\Http\Controllers\KaryawanController::class, 'delete'])->name('karyawan.delete');
 
-Route::prefix('karyawan')->group(function(){
-	Route::get('/', [App\Http\Controllers\KaryawanController::class, 'index'])->name('karyawan');
-	Route::get('create', [App\Http\Controllers\KaryawanController::class, 'create'])->name('karyawan.create');
-	Route::post('save', [App\Http\Controllers\KaryawanController::class, 'save'])->name('karyawan.save');
-	Route::get('edit/{id}', [App\Http\Controllers\KaryawanController::class, 'edit'])->name('karyawan.edit');
-	Route::post('update/{id}', [App\Http\Controllers\KaryawanController::class, 'update'])->name('karyawan.update');
-	Route::get('delete/{id}', [App\Http\Controllers\KaryawanController::class, 'delete'])->name('karyawan.delete');
+		//{id} adalah sebuah parameter yang digunakan untuk mengirim data tertentu ke dalam controller, seperti contoh yang kita kirim berupa id dari data karyawan yang nantinya digunakan untuk mencari sebuah data karyawan dengan id tertentu
+	});
 
-	//{id} adalah sebuah parameter yang digunakan untuk mengirim data tertentu ke dalam controller, seperti contoh yang kita kirim berupa id dari data karyawan yang nantinya digunakan untuk mencari sebuah data karyawan dengan id tertentu
+	Route::prefix('jabatan')->group(function(){
+		Route::get('/', [App\Http\Controllers\JabatanController::class, 'index'])->name('jabatan'); // awal route, terutama prefix harus diawali /
+		Route::get('create', [App\Http\Controllers\JabatanController::class, 'create'])->name('jabatan.create');
+		Route::post('save', [App\Http\Controllers\JabatanController::class, 'save'])->name('jabatan.save'); // save harus pake post!!	
+		Route::get('edit/{id}',[App\Http\Controllers\JabatanController::class, 'edit'])->name('jabatan.edit');
+		Route::post('update/{id}',[App\Http\Controllers\JabatanController::class, 'update'])->name('jabatan.update');
+		Route::get('delete/{id}',[App\Http\Controllers\JabatanController::class, 'delete'])->name('jabatan.delete');
+	});
 });
-
-Route::prefix('jabatan')->group(function(){
-	Route::get('/', [App\Http\Controllers\JabatanController::class, 'index'])->name('jabatan'); // awal route, terutama prefix harus diawali /
-	Route::get('create', [App\Http\Controllers\JabatanController::class, 'create'])->name('jabatan.create');
-	Route::post('save', [App\Http\Controllers\JabatanController::class, 'save'])->name('jabatan.save'); // save harus pake post!!	
-	Route::get('edit/{id}',[App\Http\Controllers\JabatanController::class, 'edit'])->name('jabatan.edit');
-	Route::post('update/{id}',[App\Http\Controllers\JabatanController::class, 'update'])->name('jabatan.update');
-	Route::get('delete/{id}',[App\Http\Controllers\JabatanController::class, 'delete'])->name('jabatan.delete');
-});
-
-
 
