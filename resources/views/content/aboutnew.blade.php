23bc1bc10414eff4a7211edef28d81cccbc5c91e
Abdurrahman <abdurrahmandean.ad@gmail.com>
2022-01-28 13:32:35 +0700

    newmerge

--- resources/views/content/aboutnew.blade.php
+++ resources/views/content/aboutnew.blade.php
@@ -49,7 +49,7 @@
                 </div>
                 <div class="col-sm-3 col-xs-6 col-sm-push-6 g-hor-centered-row__col g-margin-b-60--xs g-margin-b-0--md">
                     <div class="wow fadeInRight" data-wow-duration=".3" data-wow-delay=".1s">
-                        <img class="img-responsive" src={{asset("comrpo/img/400x500/06.jpg")}} alt="Image">
+                        <img class="img-responsive" src={{asset("compro/img/400x500/06.jpg")}} alt="Image">
                     </div>
                 </div>
                 <div class="col-sm-1"></div>
@@ -133,68 +133,24 @@
             </div>
         </div>
         <!-- End Process -->
-
-        <!-- News -->
-        <div class="g-bg-color--sky-light">
-            <div class="container g-padding-y-80--xs g-padding-y-125--sm">
-                <div class="g-text-center--xs g-margin-b-80--xs">
-                    <p class="text-uppercase g-font-size-14--xs g-font-weight--700 g-color--primary g-letter-spacing--2 g-margin-b-25--xs">Blog</p>
-                    <h2 class="g-font-size-32--xs g-font-size-36--md g-letter-spacing--1">Latest News</h2>
+        <!-- Subscribe -->
+        <div class="g-bg-color--dark-light">
+            <div class="g-container--sm g-text-center--xs g-padding-y-80--xs g-padding-y-125--sm">
+                <div class="g-margin-b-60--xs">
+                    <p class="text-uppercase g-font-size-14--xs g-font-weight--700 g-color--white-opacity g-letter-spacing--2 g-margin-b-25--xs">Subscribe</p>
+                    <h2 class="g-font-size-32--xs g-font-size-36--sm g-letter-spacing--1 g-color--white">Join Over 1000+ People</h2>
                 </div>
                 <div class="row">
-                    <div class="col-sm-4 g-margin-b-30--xs g-margin-b-0--md">
-                        <!-- News -->
-                        <article>
-                            <img class="img-responsive" src={{asset("compro/img/970x970/01.jpg")}} alt="Image">
-                            <div class="g-bg-color--white g-box-shadow__dark-lightest-v2 g-text-center--xs g-padding-x-40--xs g-padding-y-40--xs">
-                                <p class="text-uppercase g-font-size-14--xs g-font-weight--700 g-color--primary g-letter-spacing--2">News</p>
-                                <h3 class="g-font-size-22--xs g-letter-spacing--1"><a href="#">Create Something Great.</a></h3>
-                                <p>The time has come to bring those ideas and plans to life.</p>
-                            </div>
-                        </article>
-                        <!-- End News -->
+                    <div class="col-sm-6 col-sm-offset-3 col-xs-10 col-xs-offset-1">
+                        <form class="input-group">
+                            <input type="email" class="form-control s-form-v1__input g-radius--left-50" name="email" placeholder="Enter your email">
+                            <span class="input-group-btn">
+                                <button type="submit" class="s-btn s-btn-icon--md s-btn-icon--white-brd s-btn--white-brd g-radius--right-50"><i class="ti-arrow-right"></i></button>
+                            </span>
+                        </form>
                     </div>
-                    <div class="col-sm-4 g-margin-b-30--xs g-margin-b-0--md">
-                        <!-- News -->
-                        <article>
-                            <img class="img-responsive" src={{asset("compro/img/970x970/02.jpg")}} alt="Image">
-                            <div class="g-bg-color--white g-box-shadow__dark-lightest-v2 g-text-center--xs g-padding-x-40--xs g-padding-y-40--xs">
-                                <p class="text-uppercase g-font-size-14--xs g-font-weight--700 g-color--primary g-letter-spacing--2">News</p>
-                                <h3 class="g-font-size-22--xs g-letter-spacing--1"><a href="#">Jacks of All. Experts in All.</a></h3>
-                                <p>The time has come to bring those ideas and plans to life.</p>
-                            </div>
-                        </article>
-                        <!-- End News -->
-                    </div>
-                    <div class="col-sm-4">
-                        <!-- News -->
-                        <article>
-                            <img class="img-responsive" src={{asset("compro/img/970x970/03.jpg")}} alt="Image">
-                            <div class="g-bg-color--white g-box-shadow__dark-lightest-v2 g-text-center--xs g-padding-x-40--xs g-padding-y-40--xs">
-                                <p class="text-uppercase g-font-size-14--xs g-font-weight--700 g-color--primary g-letter-spacing--2">News</p>
-                                <h3 class="g-font-size-22--xs g-letter-spacing--1"><a href="#">Finding your Perfect Place.</a></h3>
-                                <p>The time has come to bring those ideas and plans to life.</p>
-                            </div>
-                        </article>
-                        <!-- End News -->
-                        <!-- Subscribe -->
-                        <div class="g-bg-color--dark-light">
-                            <div class="g-container--sm g-text-center--xs g-padding-y-80--xs g-padding-y-125--sm">
-                                <div class="g-margin-b-60--xs">
-                                    <p class="text-uppercase g-font-size-14--xs g-font-weight--700 g-color--white-opacity g-letter-spacing--2 g-margin-b-25--xs">Subscribe</p>
-                                    <h2 class="g-font-size-32--xs g-font-size-36--sm g-letter-spacing--1 g-color--white">Join Over 1000+ People</h2>
-                                </div>
-                                <div class="row">
-                                    <div class="col-sm-6 col-sm-offset-3 col-xs-10 col-xs-offset-1">
-                                        <form class="input-group">
-                                            <input type="email" class="form-control s-form-v1__input g-radius--left-50" name="email" placeholder="Enter your email">
-                                            <span class="input-group-btn">
-                                                <button type="submit" class="s-btn s-btn-icon--md s-btn-icon--white-brd s-btn--white-brd g-radius--right-50"><i class="ti-arrow-right"></i></button>
-                                            </span>
-                                        </form>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <!-- End Subscribe -->                        
+                </div>
+            </div>
+        </div>   
+        <!-- End Subscribe -->                   
 @endsection
